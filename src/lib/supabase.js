b404c19fab8b6f3808f738d9e175147a7fcd6d46
collagewebsite <jay473039@gmail.com>
2026-03-15 13:43:51 +0530

    clean commit without secrets

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://bptsqxbacpqhnckfavfx.supabase.co";
-const supabaseKey = "sb_secret_02OmCAyDOzQ4BR9eUhdjwg_iUSaAdht";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
